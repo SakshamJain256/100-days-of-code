142661b3782427f3ce68e2c972fb4c5942835bd0
SakshamJain256 <sakshamjain73919@gmail.com>
2025-11-16 00:05:16 +0530

    Create d23q1.c

---
+++ d23q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, num = 2, den = 3;
+    float sum = 0.0;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        sum += (float)num / den;
+        num += 2;
+        den += 4;
+    }
+
+    printf("Approximate sum: %.2f", sum);
+    return 0;
+}
