ead1d89260eaca088ca02381c1750a5a562570d3
SakshamJain256 <sakshamjain73919@gmail.com>
2025-11-16 00:37:01 +0530

    Create d39q2.c

---
+++ d39q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, sum = 0;
+    scanf("%d", &n);
+
+    int mat[n][n];
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &mat[i][j]);
+
+    for(i = 0; i < n; i++)
+        sum += mat[i][i];
+
+    printf("%d", sum);
+    return 0;
+}
