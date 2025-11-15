7d979ee3eaea8bc5bd549b5ee2c6a1cac5254911
SakshamJain256 <sakshamjain73919@gmail.com>
2025-11-16 00:36:42 +0530

    Create d36q2.c

---
+++ d36q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int m, n, i, j, sum = 0;
+    scanf("%d %d", &m, &n);
+
+    int mat[m][n];
+    for(i = 0; i < m; i++)
+        for(j = 0; j < n; j++) {
+            scanf("%d", &mat[i][j]);
+            sum += mat[i][j];
+        }
+
+    printf("%d", sum);
+    return 0;
+}
